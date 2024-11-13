fe46ae4ef22379728620a8ea9e275c88b400c760
Dương Thanh Bình <167697082+binhdayne@users.noreply.github.com>
2024-11-13 22:48:57 +0700

    Add files via upload

---
+++ src/main/java/com/qlthuvien/MainApp.java
@@ -0,0 +1,23 @@
+package com.qlthuvien;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FirstScreen.fxml"));
+        primaryStage.setTitle("Library Management System");
+        primaryStage.setScene(new Scene(root, 1100, 600));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
