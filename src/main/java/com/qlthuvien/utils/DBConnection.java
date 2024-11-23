0804c3a0308b973e5fda22dc31fa854e2035a6ef
hipznguyen14 <23020534@vnu.edu.vn>
2024-11-24 02:06:43 +0700

    Add files via upload

---
+++ src/main/java/com/qlthuvien/utils/DBConnection.java
@@ -0,0 +1,20 @@
+package com.qlthuvien.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/slibrary";
+    private static final String USER = "root";
+    private static final String PASSWORD = "0936494119hiep";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
---
+++ src/main/java/com/qlthuvien/utils/DatabaseTask.java
@@ -0,0 +1,48 @@
+package com.qlthuvien.utils;
+
+import javafx.concurrent.Task;
+
+import java.util.concurrent.Callable;
+import java.util.function.Consumer;
+
+public class DatabaseTask<T> extends Task<T> {
+
+    private final Callable<T> databaseQuery; // Callable để thực hiện truy vấn
+    private final Consumer<T> onSuccess; // Hàm xử lý khi truy vấn thành công
+    private final Consumer<Throwable> onError; // Hàm xử lý khi có lỗi
+
+    public DatabaseTask(Callable<T> databaseQuery, Consumer<T> onSuccess, Consumer<Throwable> onError) {
+        this.databaseQuery = databaseQuery;
+        this.onSuccess = onSuccess;
+        this.onError = onError;
+    }
+
+    @Override
+    protected T call() throws Exception {
+        // trễ 6s
+        Thread.sleep(1000);
+        // Thực hiện truy vấn cơ sở dữ liệu trong luồng riêng
+        return databaseQuery.call();
+    }
+
+    @Override
+    protected void succeeded() {
+        super.succeeded();
+        // Khi thành công, gọi hàm onSuccess để cập nhật giao diện
+        onSuccess.accept(getValue());
+    }
+
+    @Override
+    protected void failed() {
+        super.failed();
+        // Khi thất bại, gọi hàm onError để xử lý lỗi
+        onError.accept(getException());
+    }
+
+    public static <T> void run(Callable<T> query, Consumer<T> onSuccess, Consumer<Throwable> onError) {
+        DatabaseTask<T> task = new DatabaseTask<>(query, onSuccess, onError);
+        Thread thread = new Thread(task);
+        thread.setDaemon(true);
+        thread.start();
+    }
+}
