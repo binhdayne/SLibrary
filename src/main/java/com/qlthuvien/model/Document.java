0e10346082006ff295fcdafe11b8666a481b0f32
hipznguyen14 <23020534@vnu.edu.vn>
2024-11-24 05:32:31 +0700

    Update Document.java by adding equals and hashcode methods
    Help resolve the AssertionFailedError in  tests

--- src/main/java/com/qlthuvien/model/Document.java
+++ src/main/java/com/qlthuvien/model/Document.java
@@ -1,41 +1,13 @@
 package com.qlthuvien.model;
 
+import java.util.Objects;
+
 public abstract class Document {
     private int id;
     private String title;
     private String author;
     private String status;
 
-    /**
-     * Constructor Document with 0 parameters.
-     */
-    public Document() {
-        this.id = 0;
-        this.title = "";
-        this.author = "";
-        this.status = "";
-    }
-
-    /**
-     * Constructor Document with 3 parameters.
-     * @param title tieu de
-     * @param author tac gia
-     * @param status trang thai
-     */
-    public Document(String title, String author, String status) {
-        super();
-        this.title = title;
-        this.author = author;
-        this.status = status;
-    }
-
-    /**
-     * Constructor Document with 4 parameters.
-     * @param id id
-     * @param title tieu de
-     * @param author tac gia
-     * @param status trang thai
-     */
     public Document(int id, String title, String author, String status) {
         this.id = id;
         this.title = title;
@@ -77,6 +49,31 @@ public abstract class Document {
     }
 
     public abstract void printInfo();
+
+    /**
+     * Override the equals method to
+     * ensure that two equal objects are considered equal.
+     * @param o the object to compare with
+     * @return true if the objects are equal, false otherwise
+     */
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Document)) return false;
+        Document document = (Document) o;
+        return id == document.id &&
+               Objects.equals(title, document.title) &&
+               Objects.equals(author, document.author) &&
+               Objects.equals(status, document.status);
+    }
+
+    /**
+     * Override the hashCode method to
+     * ensure that two equal objects have the same hash code.
+     * @return the hash code of the object
+     */
+    public int hashCode() {
+        return Objects.hash(id, title, author, status);
+    }
 }
 
 
