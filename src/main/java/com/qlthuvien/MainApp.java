3fd749d00bb294d672d1b0789aceac4bebf29ae1
Dương Thanh Bình <167697082+binhdayne@users.noreply.github.com>
2024-11-24 19:46:10 +0700

    Update Logo for MainApp.java

--- src/main/java/com/qlthuvien/MainApp.java
+++ src/main/java/com/qlthuvien/MainApp.java
@@ -4,21 +4,30 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        // Fixing URL which is suitable for view_manager.
+        // Tải file fxml
         Parent root = FXMLLoader.load(getClass().getResource("/com/qlthuvien/view_manager/FirstScreen.fxml"));
-        primaryStage.setTitle("Library Management System");
+
+        // Đặt tên cho phần mềm
+        primaryStage.setTitle("SLibrary Login");
+
+        // Đặt kích thước
         primaryStage.setScene(new Scene(root, 835, 520));
+
+        // Load logo cho phần mềm
+        Image logo = new Image(getClass().getResourceAsStream("/icons/logo.jpg"));
+        primaryStage.getIcons().add(logo);
         primaryStage.show();
     }
 
+    // Chạy.
     public static void main(String[] args) {
         launch(args);
     }
 }
-
