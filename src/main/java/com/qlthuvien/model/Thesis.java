99ea2aae81ec46796c2e6db740e9fe718fd5dd60
Dương Thanh Bình <167697082+binhdayne@users.noreply.github.com>
2024-11-24 23:43:55 +0700

    update all model

--- src/main/java/com/qlthuvien/model/Thesis.java
+++ src/main/java/com/qlthuvien/model/Thesis.java
@@ -1,5 +1,7 @@
 package com.qlthuvien.model;
 
+import java.util.Objects;
+
 public class Thesis extends Document {
     private String supervisor;
     private String university;
@@ -46,6 +48,26 @@ public class Thesis extends Document {
     public void printInfo() {
         System.out.println("Thesis [ID=" + getId() + ", Title=" + getTitle() + ", Author=" + getAuthor() +
                 ", Supervisor=" + supervisor + ", University=" + university + ", Status=" + getStatus() + "]");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Thesis)) return false;
+        
+        Thesis thesis = (Thesis) o;
+        return getId() == thesis.getId() &&
+               Objects.equals(getTitle(), thesis.getTitle()) &&
+               Objects.equals(getAuthor(), thesis.getAuthor()) &&
+               Objects.equals(getStatus(), thesis.getStatus()) &&
+               Objects.equals(getSupervisor(), thesis.getSupervisor()) &&
+               Objects.equals(getUniversity(), thesis.getUniversity());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getTitle(), getAuthor(), getStatus(), 
+                           getSupervisor(), getUniversity());
     }
 }
 
